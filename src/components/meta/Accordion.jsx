1d32859f81c7b51f7f201a808b214cbef6830e23
Cordova <mac55181@IT-AL06-1012.local>
2017-05-03 15:19:48 -0700

    A rough version of the accordion

--- src/components/meta/Accordion.jsx
+++ src/components/meta/Accordion.jsx
@@ -3,19 +3,28 @@ import React, { Component } from 'react';
 
 class MetaAccordion extends Component {
   render() {
+    console.log(this.props.bodyItems);
+    const AccordionBody = this.props.bodyItems.map((Item) => (
+    <div>
+      <dt className="accordion__header">{Item.heading}</dt>
+      <dd className="accordion__content">
+        <p>{Item.body}</p>
+      </dd>
+    </div>));
+
     return (
       <dl className="accordion">
-        <dt className="accordion__header">Test1</dt>
-        <dd className="accordion__content">
-          <p>She sells seashells by the seashore</p>
-        </dd>
-        <dt className="accordion__header">Test 2</dt>
-        <dd className="accordion__content">
-          <p>The quick brown fox jumps over the lazy dog.</p>
-        </dd>
+        {AccordionBody}
       </dl>
     );
   }
 }
+
+MetaAccordion.propTypes = {
+  bodyItems: React.PropTypes.arrayOf(React.PropTypes.shape({
+    heading: React.PropTypes.string,
+    body: React.PropTypes.string,
+  })).isRequired,
+};
 
 export default MetaAccordion;
