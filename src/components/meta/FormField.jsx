e8d71dd2cc4a7cde39ccd7ef740b723a6dd1c6df
Moore <kevin.moore.138@gmail.com>
2017-09-07 16:30:20 -0700

    wip: form function support

--- src/components/meta/FormField.jsx
+++ src/components/meta/FormField.jsx
@@ -1,52 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { generate as shortIdGenerate } from 'shortid';
-import MetaColumn from './Column';
 import PropTypes from 'prop-types';
+import MetaColumn from './Column';
 
-const buildInput = (field) => {
-  switch (field.inputType) {
-    case 'input':
-      return <input type={field.type} id={field.id} name={field.id} placeholder={field.placeholder} />;
-    case 'textarea':
-      return <textarea id={field.id} name={field.id} placeholder={field.placeholder} />;
-    case 'select':
-      return (<select name={field.id} id={field.id}>
-        {field.selectOptions.map(option => <option value={option.value} key={shortIdGenerate()}>{option.text}</option>)}
-      </select>);
-    case 'datepicker':
-      return <input type={field.type} id={field.id} name={field.id} placeholder={field.placeholder} className="datepicker" />;
-    default:
-      return null;
-  }
+const MetaFormField = (props) => {
+  const buildInput = (field, changeFunction, value) => {
+    switch (field.inputType) {
+      case 'input':
+        return (
+          <input
+            type={field.type}
+            id={field.id}
+            name={field.id}
+            placeholder={field.placeholder}
+            value={value}
+            onChange={changeFunction}
+          />
+        );
+      case 'textarea':
+        return <textarea id={field.id} name={field.id} placeholder={field.placeholder} />;
+      case 'select':
+        return (
+          <select name={field.id} id={field.id}>
+            {field.selectOptions.map(option => (
+              <option value={option.value} key={shortIdGenerate()}>
+                {option.text}
+              </option>
+            ))}
+          </select>
+        );
+      case 'datepicker':
+        return (
+          <input
+            type={field.type}
+            id={field.id}
+            name={field.id}
+            placeholder={field.placeholder}
+            className="datepicker"
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  const label = <label htmlFor={props.field.id}>{props.field.labelText}</label>;
+  const input = buildInput(props.field, props.changeFunction, props.value);
+
+  return (
+    <MetaColumn size={props.size}>
+      {label}
+      {input}
+    </MetaColumn>
+  );
 };
-
-class MetaFormField extends Component {
-  render() {
-    const label = <label htmlFor={this.props.field.id}>{this.props.field.labelText}</label>;
-    const input = buildInput(this.props.field);
-
-    return (
-      <MetaColumn size={this.props.size} value={this.props.value}>
-        {label}
-        {input}
-      </MetaColumn>
-    );
-  }
-}
 
 MetaFormField.defaultProps = {
   field: {},
   size: 'sm',
   value: '12',
+  changeFunction: () => {},
 };
 
 MetaFormField.propTypes = {
   field: PropTypes.shape({
     inputType: PropTypes.oneOf(['input', 'textarea', 'select', 'datepicker']).isRequired,
-    selectOptions: PropTypes.arrayOf(PropTypes.shape({
-      value: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-    })),
+    selectOptions: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+      }),
+    ),
     id: PropTypes.string.isRequired,
     labelText: PropTypes.string,
     type: PropTypes.string,
@@ -54,6 +78,7 @@ MetaFormField.propTypes = {
   }),
   size: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
+  changeFunction: PropTypes.func,
 };
 
 export default MetaFormField;
