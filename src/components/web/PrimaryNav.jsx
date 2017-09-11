31f1ba113ebe34968888e97f0b27d3b108fd603a
Moore <kevin.moore.138@gmail.com>
2017-09-11 13:47:11 -0700

    Updated nav

--- src/components/web/PrimaryNav.jsx
+++ src/components/web/PrimaryNav.jsx
@@ -11,22 +11,25 @@ const WebPrimaryNav = () => (
         <div className="collapse navbar-collapse navbar-ex1-collapse">
           <ul className="nav navbar-nav navbar-center">
             <li>
-              <a href="http://www.csun.edu/faculty">Home</a>
+              <a href="http://www.csun.edu/faculty">About IT</a>
             </li>
             <li className="active">
-              <a href="http://www.csun.edu/faculty/profiles">Profiles</a>
+              <a href="http://www.csun.edu/faculty/profiles">Need Help?</a>
             </li>
             <li className="">
-              <a href="http://www.csun.edu/faculty/stories">Stories</a>
+              <a href="http://www.csun.edu/faculty/stories">Academic Technology</a>
             </li>
             <li className="">
-              <a href="http://www.csun.edu/faculty/scholarship">Scholarship</a>
+              <a href="http://www.csun.edu/faculty/scholarship">Training</a>
             </li>
             <li className="">
-              <a href="http://www.csun.edu/faculty/about/version-history">About</a>
+              <a href="http://www.csun.edu/faculty/about/version-history">Services</a>
             </li>
             <li className="">
-              <a href="https://www.csun.edu/faculty/login">Login</a>
+              <a href="https://www.csun.edu/faculty/login">Information Security</a>
+            </li>
+            <li className="">
+              <a href="https://www.csun.edu/faculty/login">Zoom</a>
             </li>
           </ul>
         </div>
