eae15d940b155000b4a1816cd9ef52574c1d9f6f
Rene Garcia-Ayon <rene.garciaayon.646@my.csun.edu>
2017-10-25 16:14:46 -0700

    Documentation for Form Fields & Form Groups

---
+++ dev/public/pages/forms.jsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import { MetaContainer, MetaColumn, MetaRow, MetaFormField, MetaFormGroup } from '../../../src';
+
+const DocForms = () => {
+  const code = `import MetaFormField from 'csun-react-components';
+import MetaFormGroup from 'csun-react-components';
+
+<MetaFormGroup>
+  //Input Field
+  <MetaFormField
+    field={{
+      inputType: 'input',
+      id: 'inputFied',
+      placeholder: 'Type a message...',
+      type: 'text',
+      autoComplete: 'off',
+    }}
+    value={''}
+  />
+
+  //Text Area
+  <MetaFormField
+    field={{
+      inputType: 'textarea',
+      id: 'textFied',
+      placeholder: 'Type a message...',
+      type: 'text',
+      autoComplete: 'off',
+    }}
+    value={''}
+  />
+
+  //Select Field
+  <MetaFormField
+    field={{
+      inputType: 'select',
+      id: 'selectField',
+      type: 'checkbox',
+      selectOptions: [
+        {
+          value: '1',
+          text: 'Example 1',
+        },
+        {
+          value: '2',
+          text: 'Example 2',
+        },
+      ],
+    }}
+    value={''}
+  />
+
+  //Date Picker
+  <MetaFormField
+    field={{
+      inputType: 'datepicker',
+      id: 'datepicker',
+      type: 'date',
+    }}
+    value={''}
+  />
+</MetaFormGroup>
+    `;
+
+  const propTypesTitle = '// Prop Types For MetaFormField';
+  const propTypes = `
+  field: PropTypes.shape({
+    inputType: PropTypes.oneOf(['input', 'textarea', 'select', 'datepicker']).isRequired,
+    //selectOptions only needs to be called when using inputType 'select'
+    selectOptions: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
+      }),
+    ),
+    id: PropTypes.string.isRequired,
+    labelText: PropTypes.string,
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    autoComplete: PropTypes.string,
+  }),
+  className: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  changeFunction: PropTypes.func,
+  
+  `;
+
+  const propTypesTitleTwo = '// Prop Types For MetaFormGroup';
+  const propTypesTwo = `
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  submitFunction: PropTypes.func,
+  `;
+
+  return (
+    <MetaContainer>
+      <MetaRow>
+        <MetaColumn size={'sm'} value={'9'}>
+          <MetaFormGroup>
+            <h2>Input Field</h2>
+            <MetaFormField
+              field={{
+                inputType: 'input',
+                id: 'inputFied',
+                placeholder: 'Type a message...',
+                type: 'text',
+                autoComplete: 'off',
+              }}
+              value={''}
+            />
+            <br />
+            <h2>Text Area</h2>
+            <MetaFormField
+              field={{
+                inputType: 'textarea',
+                id: 'textFied',
+                placeholder: 'Type a message...',
+                type: 'text',
+                autoComplete: 'off',
+              }}
+              value={''}
+            />
+            <br />
+            <h2>Select</h2>
+            <MetaFormField
+              field={{
+                inputType: 'select',
+                id: 'selectField',
+                type: 'checkbox',
+                selectOptions: [
+                  {
+                    value: '1',
+                    text: 'Example 1',
+                  },
+                  {
+                    value: '2',
+                    text: 'Example 2',
+                  },
+                ],
+              }}
+              value={''}
+            />
+            <br />
+            <h2>Date Picker</h2>
+            <MetaFormField
+              field={{
+                inputType: 'datepicker',
+                id: 'datepicker',
+                type: 'date',
+              }}
+              value={''}
+            />
+            <br />
+          </MetaFormGroup>
+          <pre>
+            <code>
+              {code}
+              <div className="type--dark-gray">
+                <strong>{propTypesTitle}</strong>
+                {propTypes}
+                <strong>{propTypesTitleTwo}</strong>
+                {propTypesTwo}
+              </div>
+            </code>
+          </pre>
+        </MetaColumn>
+      </MetaRow>
+    </MetaContainer>
+  );
+};
+
+export default DocForms;
