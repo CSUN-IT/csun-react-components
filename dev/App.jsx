051d489e2fef861c9b86da6b42763aa338d64702
Moore <kevin.moore.138@gmail.com>
2017-11-02 10:57:01 -0700

    Removed dummy web-one header from main

--- dev/App.jsx
+++ dev/App.jsx
@@ -22,38 +22,9 @@ const App = () => {
     title: 'CSUN Metaphor React Components',
   };
 
-  const menuItems2 = [
-    {
-      title: 'About IT',
-      to: 'https://www.csun.edu/it/about',
-      external: true,
-    },
-    {
-      title: 'IT',
-      to: 'it',
-      external: true,
-    },
-    {
-      title: 'Giving',
-      to: 'giving',
-      external: true,
-    },
-  ];
-
-  const secondaryWordmark = {
-    to: 'https://www.csun.edu/it',
-    title: 'Information\nTechnology',
-  };
-
-  const tertiaryWordmark = {
-    to: 'https://www.csun.edu/it',
-    title: 'Information\nTechnology',
-  };
-
   return (
     <Router>
       <div>
-        <WebHeader secondaryWordmark={secondaryWordmark} tertiaryWordmark={tertiaryWordmark} menuItems={menuItems2} />
         <MetaPrimaryNav menuItems={menuItems} navColor={'cream'} subBrand={subBrand} />
         <Route path="/docs" component={DocLayout} />
       </div>
