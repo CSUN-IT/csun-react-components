5948f974a9b2c02dd8d14ca2b09f756763e8c6cf
HeyItsMario <mario.cordova.862@my.csun.edu>
2018-02-05 16:51:33 -0800

    Version 1.0 of the card. Need the buttons on the bottom

--- dev/public/pages/webCardlist.jsx
+++ dev/public/pages/webCardlist.jsx
@@ -74,7 +74,7 @@ const DocCardList = (props) => {
         </MetaColumn>
         <MetaColumn sizes={['sm']} values={['12']}>
           <WebCardList>
-            <WebCard title={'My clever title'} content={'Here is content.'} img={'test'} btn={'support'} />
+            <WebCard title={'Title'} content={'This here is content'} img={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png'} />
           </WebCardList>
         </MetaColumn>
       </MetaRow>
--- src/components/web/Card.jsx
+++ src/components/web/Card.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = (props) => {
+  const title = props.title ? <h3>{props.title}</h3> : null;
+  const contents = props.content ? <p>{props.content}</p> : null;
+  const image = props.img ? <img src={props.img} alt={'alt text'} height={'100'} width={'300'} /> : null;
   return (
-    <p>Hello this is a card.</p>
+    <div className={props.className}>
+      {image}
+      {title}
+      {contents}
+    </div>
   );
 };
 
 Card.defaultProps = {
   className: '',
+  title: null,
+  content: null,
+  img: null,
 };
 
 Card.propTypes = {
   className: PropTypes.string,
+  title: PropTypes.string,
+  content: PropTypes.string,
+  img: PropTypes.string,
 };
 
 export default Card;
