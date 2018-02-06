fb4fe3b6f093e190707d5036164a295735aa50dc
HeyItsMario <mario.cordova.862@my.csun.edu>
2018-02-06 14:07:05 -0800

    web cards small changes

--- dev/public/pages/webCardlist.jsx
+++ dev/public/pages/webCardlist.jsx
@@ -2,69 +2,6 @@ import React from 'react';
 import { MetaContainer, MetaColumn, MetaRow, WebCardList, WebCard } from '../../../src';
 
 const DocCardList = (props) => {
-  const code = `import Webheader from 'csun-components';
-
-      const menuItems = [
-      {
-        title: 'Home',
-        to: '',
-        exact: true,
-      },
-      {
-        title: 'IT',
-        to: 'it',
-      },
-      {
-        title: 'Giving',
-        to: 'giving',
-      },
-    ];
-    const secondaryWordmark = {
-      to: 'https://www.csun.edu/it',
-      title: 'Information\nTechnology',
-    };
-  
-    const tertiaryWordmark = {
-      to: 'https://www.csun.edu/it',
-      title: 'AI',
-    };
-    
-    <WebHeader
-      secondaryWordmark={secondaryWordmark}
-      tertiaryWordmark={tertiaryWordmark}
-      menuItems={menuItems}
-    />
-    `;
-
-  const propTypesTitle = '// Prop Types';
-  const propTypes = `
-  WebHeader.defaultProps = {
-    secondaryWordmark: null,
-    tertiaryWordmark: null,
-    showMobileNav: false,
-    menuItems: null,
-    id: null,
-  };
-  
-  WebHeader.propTypes = {
-    id: PropTypes.string,
-    secondaryWordmark: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      to: PropTypes.string.isRequired,
-    }),
-    tertiaryWordmark: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      to: PropTypes.string.isRequired,
-    }),
-    menuItems: PropTypes.arrayOf(
-      PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        to: PropTypes.string.isRequired,
-        exact: PropTypes.bool,
-        external: PropTypes.bool,
-      }),
-    ),
-  }`;
   return (
     <MetaContainer>
       <MetaRow>
@@ -74,7 +11,7 @@ const DocCardList = (props) => {
         </MetaColumn>
         <MetaColumn sizes={['sm']} values={['12']}>
           <WebCardList>
-            <WebCard title={'Title'} content={'This here is content'} img={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png'} />
+            <WebCard title={'Title'} content={'This here is content'} img={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png'} linkTo={'https://www.google.com'} />
           </WebCardList>
         </MetaColumn>
       </MetaRow>
--- src/components/web/Card.jsx
+++ src/components/web/Card.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './sass/_webCard.scss';
 
 const Card = (props) => {
   const title = props.title ? <h3>{props.title}</h3> : null;
   const contents = props.content ? <p>{props.content}</p> : null;
   const image = props.img ? <img src={props.img} alt={'alt text'} height={'100'} width={'300'} /> : null;
+  const btn = props.linkTo ? (typeof props.linkTo === 'string' ? <a href={props.linkTo}>Button</a> : <a>Arr Button</a>) : null;
   return (
-    <div className={props.className}>
+    <div className={`${props.className} cardz`}>
       {image}
       {title}
       {contents}
+      {btn}
     </div>
   );
 };
@@ -19,6 +22,7 @@ Card.defaultProps = {
   title: null,
   content: null,
   img: null,
+  linkTo: null,
 };
 
 Card.propTypes = {
@@ -26,6 +30,7 @@ Card.propTypes = {
   title: PropTypes.string,
   content: PropTypes.string,
   img: PropTypes.string,
+  linkTo: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)])
 };
 
 export default Card;
