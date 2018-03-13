ed021076f2e696277bc36a2eef19742c0f3a9623
ethanleiter <ethan.leiter.522@my.csun.edu>
2018-03-13 09:34:49 -0700

    Improve variable names and update tables example
    Improves MetaTables variable names so they are better understood and updated the MetaTables example for CSUN React Components.

--- dev/public/pages/tables.jsx
+++ dev/public/pages/tables.jsx
@@ -12,27 +12,23 @@ import {
 } from '../../../src';
 
 const DocTables = () => {
-  const importCode =
-    'import { MetaTable, MetaTableHead, MetaTableRow, MetaTableBody, MetaTableHeaderCell, MetaTableCell } from csun-react-components\n';
-  const code = `\n\n<MetaTable>
-  <MetaTableHead>
-    <MetaTableRow>
-      <MetaTableHeaderCell> Hello </MetaTableHeaderCell>
-      <MetaTableHeaderCell> World </MetaTableHeaderCell>
-    </MetaTableRow>
-  </MetaTableHead>
-  <MetaTableBody>
-    <MetaTableRow>
-      <MetaTableCell> Foo </MetaTableCell>
-      <MetaTableCell> Bar </MetaTableCell>
-    </MetaTableRow>
-  </MetaTableBody>
-</MetaTable>`;
+  const importCode = 'import { MetaTable } from csun-react-components\n';
+  const code = `<MetaTable
+  header={['Test Header', 'Test Header 2']}
+  dataCells={[
+    'Test Child',
+    'Test Child 2',
+    'Test Child 3',
+    'Test Child 4',
+    'Test Child 5',
+    'Test Child 6',
+  ]}
+/>`;
 
   const propTypesTitle = '\n\nProp Types:\n';
   const propTypes = `  className: PropTypes.string,
-  header: PropTypes.arrayOf(PropTypes.string),
-  body: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string).isRequired).isRequired,`;
+  header: PropTypes.arrayOf(PropTypes.string).isRequired,
+  dataCells: PropTypes.arrayOf(PropTypes.string).isRequired,`;
 
   return (
     <MetaContainer>
@@ -42,38 +38,42 @@ const DocTables = () => {
           <hr />
         </MetaColumn>
       </MetaRow>
-      <MetaRow>
-        <MetaColumn sizes={['sm']} values={['9']}>
-          <MetaTable
-            header={['Test Header', 'Test Header 2', 'Test Header 3', 'Test Header 4']}
-            column={[
-              'Test Child',
-              'Test Child 2',
-              'Test Child 3',
-              'Test Child 4',
-              'Test Child 5',
-              'Test Child 6',
-              'Test Child 7',
-              'Test Child 8',
-              'Test Child 9',
-              'Test Child 10',
-              'Test Child 11',
-              'Test Child 12',
-            ]}
-          />
-          <br />
-          <pre>
-            <code>
-              {importCode}
-              {code}
-              <div className="type--dark-gray">
-                <strong>{propTypesTitle}</strong>
-                {propTypes}
-              </div>
-            </code>
-          </pre>
-        </MetaColumn>
-      </MetaRow>
+      <MetaTable
+        header={['Test Header', 'Test Header 2']}
+        dataCells={[
+          'Test Child',
+          'Test Child 2',
+          'Test Child 3',
+          'Test Child 4',
+          'Test Child 5',
+          'Test Child 6',
+        ]}
+      />
+      <br />
+      <pre>
+        <code>
+          {importCode}
+          {code}
+          <div className="type--dark-gray">
+            <strong>{propTypesTitle}</strong>
+            {propTypes}
+          </div>
+        </code>
+      </pre>
+      <MetaTable
+        header={['Name', 'Type', 'Required']}
+        dataCells={[
+          'className',
+          'String',
+          'No',
+          'Header',
+          'Array',
+          'Yes',
+          'dataCells',
+          'Array',
+          'Yes',
+        ]}
+      />
     </MetaContainer>
   );
 };
--- src/components/meta/Table.jsx
+++ src/components/meta/Table.jsx
@@ -9,25 +9,12 @@ const MetaTable = (props) => {
     tableHead.push(<th key={i}>{props.header[i]}</th>);
   }
   const tableBody = [];
-  for (let i = 0; i < props.column.length / numColumns; i += 1) {
+  for (let i = 0; i < props.dataCells.length / numColumns; i += 1) {
     const rowItems = [];
     for (let j = 0; j < numColumns; j += 1) {
-      rowItems.push(<td>{props.column[i * numColumns + j]}</td>);
+      rowItems.push(<td>{props.dataCells[i * numColumns + j]}</td>);
     }
     tableBody.push(<tr>{rowItems}</tr>);
-    /*
-    if (props.column[i] === '\n') {
-      tableBody.push(
-        <tr key={i + 1}>
-          <td>{props.column[i + 1]}</td>
-          <td>{props.column[i + 2]}</td>
-          <td>{props.column[i + 3]}</td>
-        </tr>,
-      );
-    } else {
-      tableBody.push(<td key={i}>{props.column[i]}</td>);
-    }
-    */
   }
   return (
     <div className={className}>
@@ -47,7 +34,7 @@ MetaTable.defaultProps = {
 MetaTable.propTypes = {
   className: PropTypes.string,
   header: PropTypes.arrayOf(PropTypes.string).isRequired,
-  column: PropTypes.arrayOf(PropTypes.string).isRequired,
+  dataCells: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default MetaTable;
