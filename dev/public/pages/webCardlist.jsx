9e62ecb236fd695bcdb77c9413e68ce9632afe07
Pete Medina <petemedinacreative@gmail.com>
2018-04-10 13:54:06 -0700

    Add cardlist to docs

--- dev/public/pages/webCardlist.jsx
+++ dev/public/pages/webCardlist.jsx
@@ -1,39 +1,54 @@
-// import React from 'react';
-// import { MetaContainer, MetaColumn, MetaRow, WebCardList, WebCard } from '../../../src';
+import React from 'react';
+import { MetaContainer, MetaColumn, MetaRow, WebCardList, WebCard } from '../../../src';
 
-// const buttons = [
-//   {
-//     title: 'Google',
-//     link: 'https://www.google.com',
-//   },
-//   {
-//     title: 'Reddit',
-//     link: 'https://www.reddit.com',
-//   },
-//   {
-//     title: 'CSUN',
-//     link: 'https://www.csun.edu',
-//   },
-// ];
+const buttonsWebsites = [
+  {
+    title: 'Google',
+    link: 'https://www.google.com',
+  },
+  {
+    title: 'Reddit',
+    link: 'https://www.reddit.com',
+  },
+  {
+    title: 'CSUN',
+    link: 'https://www.csun.edu',
+  },
+];
 
-// const DocCardList = (props) => {
-//   return (
-//     <MetaContainer>
-//       <MetaRow>
-//         <MetaColumn sizes={['sm']} values={['12']}>
-//           <h1>Cards</h1>
-//           <hr />
-//         </MetaColumn>
-//         <MetaColumn sizes={['sm']} values={['12']}>
-//           <WebCardList>
-//             <WebCard title={'Title'} content={'This was a triumph. I\'m making a note here, huge success. It\'s hard to overstate my satisfaction.'} img={'https://upload.wikimedia.org/wikipedia/commons/c/c7/OviattLibrary.jpg'} linkTo={'https://www.csun.edu/'} buttonArray={buttons} />
-//             <WebCard title={'Title'} content={'The cake is a lie.'} img={''} linkTo={'https://www.csun.edu/'} buttonArray={buttons} />
-//             <WebCard title={'Title'} content={'This was a triumph. I\'m making a note here, huge success. It\'s hard to overstate my satisfaction.'} img={'https://upload.wikimedia.org/wikipedia/commons/c/c7/OviattLibrary.jpg'} linkTo={'https://www.csun.edu/'} buttonArray={buttons} />
-//           </WebCardList>
-//         </MetaColumn>
-//       </MetaRow>
-//     </MetaContainer>
-//   );
-// };
+const buttonsSocial = [
+    {
+      title: 'Facebook',
+      link: 'https://www.facebook.com/calstatenorthridge',
+    },
+    {
+      title: 'Twitter',
+      link: 'https://www.twitter.com/csunorthridge',
+    },
+    {
+      title: 'Instagram',
+      link: 'https://www.instagram.edu/csun_edu',
+    },
+];
 
-// export default DocCardList;
+const DocCardList = (props) => {
+  return (
+    <MetaContainer>
+      <MetaRow>
+        <MetaColumn sizes={['sm']} values={['12']}>
+          <h1>Cards</h1>
+          <hr />
+        </MetaColumn>
+        <MetaColumn sizes={['sm']} values={['12']}>
+          <WebCardList>
+            <WebCard title={'Title'} content={'This was a triumph. I\'m making a note here, huge success. It\'s hard to overstate my satisfaction.'} img={'https://upload.wikimedia.org/wikipedia/commons/c/c7/OviattLibrary.jpg'} linkTo={'https://www.csun.edu/'} buttonArray={buttonsWebsites} />
+            <WebCard title={'New Title'} content={'The cake is a lie.'} img={''} linkTo={'https://www.csun.edu/'} />
+            <WebCard title={'Another Title'} content={'This was a triumph. I\'m making a note here, huge success. It\'s hard to overstate my satisfaction.'} img={'https://upload.wikimedia.org/wikipedia/commons/c/c7/OviattLibrary.jpg'} linkTo={'https://www.csun.edu/'} buttonArray={buttonsSocial} />
+          </WebCardList>
+        </MetaColumn>
+      </MetaRow>
+    </MetaContainer>
+  );
+};
+
+export default DocCardList;
--- src/components/web/CardList.jsx
+++ src/components/web/CardList.jsx
@@ -1,39 +1,24 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// const CardList = (props) => {
+const CardList = (props) => {
 
-//   constructor() {
-//     super();
-//     this.state = {
-//         sidbarPushCollapsed: false,
-//         profileCollapsed: false
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-//   handleClick() {
-//       this.setState({
-//           sidbarPushCollapsed: !this.state.sidbarPushCollapsed,
-//           profileCollapsed: !this.state.profileCollapsed
-//       });
-//   }
+  return (
+    <div>
+      <div className={props.className}>
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
-//   return (
-//     <div>
-//       <div className={props.className}>
-//         {props.children}
-//       </div>
-//     </div>
-//   );
-// };
+CardList.defaultProps = {
+  className: 'card-list',
+};
 
-// CardList.defaultProps = {
-//   className: 'card-list',
-// };
+CardList.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
 
-// CardList.propTypes = {
-//   className: PropTypes.string,
-//   children: PropTypes.node.isRequired,
-// };
-
-// export default CardList;
+export default CardList;
